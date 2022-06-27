174eb0a688762daa7fce09000bcf0ab5cd4ad53e
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-27 16:03:09 +0100

    update routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const booksController = require('../controllers/Users')
+const usersController = require('../controllers/Users')
 
 router.get('/', userController.index)
 router.get('/:id', userController.show)
 router.post('/', userController.create)
+// router.get('/username/', verifyToken, usersController.getByUsername)
+// router.get('/', usersController.getById);
+
+
 
 module.exports = router;
+
+
+
+
