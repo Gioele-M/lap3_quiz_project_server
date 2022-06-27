3394b9ab8299d1c308eabd2a1991818fc46e211e
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-27 15:59:20 +0100

    add Users models

--- models/User.js
+++ models/User.js
@@ -26,5 +26,47 @@ module.exports = class User {
         })
     }
 
-
+    static create({ username, email, password }){
+        return new Promise(async (resolve, reject) => {
+            try {
+                console.log(username)
+                const result = await db.query('INSERT INTO users (username, email, password_digest) VALUES ($1, $2, $3) RETURNING *;', [ username, email, password ]);
+                const user = new User(result.rows[0]);
+                resolve(user)
+            } catch (err) {
+                reject(`Error creating user: ${err}`)
+            }
+        })
+    }
+    static findByUsername(username){
+        return new Promise (async (resolve, reject) => {
+            try {
+                
+                let userData = await db.query('SELECT * FROM users WHERE username = $1;', [ username ]);
+                let user = new User(userData.rows[0]);
+                resolve(user);
+            } catch (err) {
+                reject('User not found');
+            };
+        });
+    };
+ 
+    static findByEmail(email){
+        return new Promise(async (res, rej) => {
+            try {
+                let result = await db.query('SELECT * FROM users WHERE email = $1;', [ email ]);
+                let user = new User(result.rows[0])
+                res(user)
+            } catch (err) {
+                rej(`Error retrieving user: ${err}`)
+            }
+        })
+    }
 }
+
+
+module.exports = Users
+
+
+
+
