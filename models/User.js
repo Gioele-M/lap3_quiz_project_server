fb65ea72ba53c1d6ba6f9ad706bbae33ab1d9276
Gioele-M <gioelemontis97@gmail.com>
2022-06-28 12:01:03 +0100

    code cleanup

--- models/User.js
+++ models/User.js
@@ -38,20 +38,6 @@ module.exports = class User {
             }
         })
     }
-    
-    // ADD NEW USER 
-    //
-    // static addUserData(data) {
-    //     return new Promise(async (resolve, reject) => {
-    //         try {
-    //             let user = userScore;
-    //             let newUser = new User(user.rows[0])
-    //             resolve(newUser)
-    //         } catch (err) {
-    //             reject(`Error creating user: ${err}`)
-    //         }
-    //     })
-    // }
 
 
     static findByUsername(username){
@@ -106,6 +92,3 @@ module.exports = class User {
 
     
 }
-
-
-// delete fn
--- routes/users.js
+++ routes/users.js
@@ -3,25 +3,10 @@ const router = express.Router();
 const userController = require('../controllers/Users')
 
 router.get('/', userController.indexUsers)
-// router.get('/leaderboard', userController.leaderboard)
-// router.get('/:username', userController.getUser)
-// router.post('/', userController.createNewUser)
-// router.patch('/', userController.updateScore)
-
-
-
-
-
-
-
-
-// router.get('/', usersController.index)
-// router.get('/:id', usersController.show)
-// router.post('/', usersController.create)
-
-
-// router.get('/username/', verifyToken, usersController.getByUsername)
-// router.get('/', usersController.getById);
+router.get('/leaderboard', userController.leaderboard)
+router.get('/:username', userController.getUser)
+router.post('/', userController.createNewUser)
+router.patch('/', userController.updateScore)
 
 
 
