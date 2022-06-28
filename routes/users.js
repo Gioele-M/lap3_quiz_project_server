5f13b77f94d84f94f051e5fe7b662bc3b9b7c711
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-28 12:27:33 +0100

    update routes for users

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/Users')
 
 router.get('/', userController.indexUsers)
-router.get('/leaderboard', userController.leaderboard)
 router.get('/:username', userController.getUser)
-router.post('/', userController.createNewUser)
-router.patch('/', userController.updateScore)
-
 
 
 module.exports = router;
