d3fd8b4c4c224585f2b30b9b039a15814e9cfc31
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-28 08:44:47 +0100

    add controllers

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,3 +1,82 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/User");
+
+// Get all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.all;
+    res.json({ users });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+router.get("/at/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json({ user });
+    res.status(200);
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+
+// LEADERBOARD
+// router.get("/leaderboard", async (req, res) => {
+//   try {
+//     const users = await User.leaderboard;
+//     res.json({ users });
+//     res.status(200);
+//   } catch (err) {
+//     res.status(500).json({ err });
+//   }
+// });
+
+
+
+// Create User
+router.post("/", async (req, res) => {
+  try {
+    const user = await User.create(
+      req.body.username,
+      req.body.email,
+      req.body.password
+    );
+    res.json(user);
+    res.status(201);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+});
+
+//Update User
+// router.patch("/:id", async (req, res) => {
+//   try {
+//     const updatedUser = await User.update(req.body.username, req.body.score);
+//     res.json(updatedUser);
+//   } catch (err) {
+//     res.status(500).json({ err });
+//   }
+// });
+
+//Delete User
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    await user.destroy();
+    res.status(204).end();
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
+
+
 // const express = require('express');
 // const router = express.Router();
 
