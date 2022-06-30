3cc979b7929a07ca6ea26b90be88202e9d567b05
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-30 11:51:24 +0100

    add integration testing

--- test/integration/authorRoutes.spec.js
+++ test/integration/authorRoutes.spec.js
@@ -1,27 +1,31 @@
-// describe('author endpoints', () => {
-//     let api;
-//     beforeEach(async () => {
-//         await resetTestDB()
-//     });
+describe('author endpoints', () => {
+    let api;
+    beforeEach(async () => {
+        await resetTestDB()
+    });
 
-//     beforeAll(async () => {
-//         api = app.listen(5000, () => console.log('Test server running on port 5000'))
-//     });
+    beforeAll(async () => {
+        api = app.listen(5000, () => console.log('Test server running on port 5000'))
+    });
 
-//     afterAll(done => {
-//         console.log('Gracefully stopping test server')
-//         api.close(done)
-//     })
+    afterAll(done => {
+        console.log('Gracefully stopping test server')
+        api.close(done)
+    })
 
-//     it('should return a list of all authors in database', async () => {
-//         const res = await request(api).get('/authors');
-//         expect(res.statusCode).toEqual(200);
-//         expect(res.body.length).toEqual(2);
-//     })
+    it('login returns user id', async () => {
+        const res = await request(api).post('/auth/login').send({"username": "Adam10","email": "adams@gmail.com", "password": "password"})
+        expect(res.body.username).toBe("Adam10");
+    })
+
+    it('login with wrong data does not return id', async () => {
+        const res = await request(api).post('/auth/login').send({"username": "Adam10","email": "adams@gmail.com", "password": "nopass"})
+        expect(res.body.msg).toBe("Incorrect Username or Password");
+    })
 
 //     it('should return a list of books by a specific author', async () => {
 //         const res = await request(api).get('/authors/1');
 //         expect(res.statusCode).toEqual(200);
 //         expect(res.body.books.length).toEqual(2);
 //     })
-// })
+})
